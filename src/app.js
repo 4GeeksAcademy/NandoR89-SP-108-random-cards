2c3056321a0fb60ddad25b743e7d0e4fe45e4a21
Fernando R. G. <nando.rodriguez.gomez89@gmail.com>
2025-05-10 00:18:07 +0000

    se generan las cartas aleatorias

--- src/app.js
+++ src/app.js
@@ -2,10 +2,27 @@ import "bootstrap";
 import "./style.css";
 
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.generateRandomCard = function generateRandomCard() {
+
+  let symbols = ['♠', '♥', '♦', '♣'];
+  let rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
+
+  let randomSymbol = symbols[Math.floor(Math.random() * symbols.length)];
+  let randomRank = rank[Math.floor(Math.random() * rank.length)];
+
+  let symbolUp = document.querySelector('#symbol-up');
+  symbolUp.innerHTML = randomSymbol;
+
+  let symbolDown = document.querySelector('#symbol-down');
+  symbolDown.innerHTML = randomSymbol;
+
+  let modifyRank = document.querySelector('#rank');
+  modifyRank.innerHTML = randomRank;
+
+  let colorSymbol = (randomSymbol === '♥' || randomSymbol === '♦') ? 'red' : 'black';
+  symbolUp.style.color = colorSymbol;
+  symbolDown.style.color = colorSymbol;
+}
 
 
-// let symbols = [♠, ♥, ♦, ♣]
+
